71d490144fd990bcf4d870d5fef7bc24e3215d0f
amsminn <kim1109123@gmail.com>
2025-07-04 18:48:01 +0900

    refactor: apply SCREAMING_CASE convention on constant variable

--- lib/src/api/util/token_storage.dart
+++ lib/src/api/util/token_storage.dart
@@ -1,25 +1,25 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_SECURE_STORAGE/flutter_SECURE_STORAGE.dart';
 import 'dart:developer' as developer;
 
 /// JWT 토큰 저장소 - 토큰 관리만 담당
 class TokenStorage {
-  static const FlutterSecureStorage _secure_storage = FlutterSecureStorage();
+  static const FlutterSecureStorage _SECURE_STORAGE = FlutterSecureStorage();
   static const String _tokenKey = 'jwt_token';
 
   /// 토큰 저장
   static Future<void> saveToken(String token) async {
-    await _secure_storage.write(key: _tokenKey, value: token);
+    await _SECURE_STORAGE.write(key: _tokenKey, value: token);
     developer.log('토큰이 안전하게 저장되었습니다', name: 'TokenStorage');
   }
 
   /// 저장된 토큰 가져오기
   static Future<String?> getToken() async {
-    return await _secure_storage.read(key: _tokenKey);
+    return await _SECURE_STORAGE.read(key: _tokenKey);
   }
 
   /// 토큰 삭제
   static Future<void> clearToken() async {
-    await _secure_storage.delete(key: _tokenKey);
+    await _SECURE_STORAGE.delete(key: _tokenKey);
     developer.log('토큰이 삭제되었습니다', name: 'TokenStorage');
   }
 
