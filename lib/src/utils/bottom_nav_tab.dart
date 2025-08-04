32d2fd96a1f86b4570e0ef358ad8dd8c68996e3f
Mindev27 <dlrkdals23444@gmail.com>
2025-08-04 15:33:02 +0900

    [SWM-236] feat: 하단 네이게이션 바 이름 메인에서 홈으로 변경

--- lib/src/utils/bottom_nav_tab.dart
+++ lib/src/utils/bottom_nav_tab.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // 이것도 나중에 메타 데이터로 받아올 수 있을듯
 enum BottomNavTab {
-  profile('메인', Icons.person_outline, Icons.person),
+  profile('홈', Icons.person_outline, Icons.home),
   leaderboard('리더보드', Icons.leaderboard_outlined, Icons.leaderboard),
   search('검색', Icons.search_outlined, Icons.search),
   map('지도', Icons.map_outlined, Icons.map);
