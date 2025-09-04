8f6e80588054e37ea596d538350a3f16965dd784
Mindev27 <dlrkdals23444@gmail.com>
2025-09-04 17:11:50 +0900

    [SWM-310] refact: dotenv제거 및 --dart-define으로 수정

--- lib/src/api/auth.dart
+++ lib/src/api/auth.dart
@@ -9,7 +9,6 @@ import 'util/core/api_client.dart';
 import 'util/auth/token_storage.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'util/error/auth_cancelled_exception.dart';
 
 /// 인증 관련 API 호출 함수들
@@ -241,8 +240,8 @@ class AuthApi {
       final googleSignIn = GoogleSignIn.instance;
 
       // 환경 변수에서 클라이언트 ID 로드 (.env)
-      final iosClientId = dotenv.env['GOOGLE_IOS_CLIENT_ID'];
-      final webClientId = dotenv.env['GOOGLE_WEB_CLIENT_ID'];
+      const iosClientId = String.fromEnvironment('GOOGLE_IOS_CLIENT_ID');
+      const webClientId = String.fromEnvironment('GOOGLE_WEB_CLIENT_ID');
 
       await GoogleSignIn.instance.initialize(
         clientId: defaultTargetPlatform == TargetPlatform.iOS
